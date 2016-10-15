961fa544100a46624e836aa34517492f5e913914
gunnlaugur <gju@kth.se>
2016-10-15 12:35:41 +0200

    Added register stuff

---
+++ DistroLab2/DistroLab2/Database/AddUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DistroLab2.Models.Mail_Models;
+
+namespace DistroLab2.Database
+{
+    public class AddUser
+    {
+        public static bool registerUser(User user)
+        {
+
+
+            return false;
+        }
+    }
+}
---
+++ DistroLab2/DistroLab2/Models/Mail_Models/AuthenticateModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DistroLab2.Models.Mail_Models
+{
+    public class AuthenticateModel
+    {
+        public AuthenticateModel()
+        {
+
+        }
+
+        public bool Register(ApplicationUser user)
+        {
+            User mailUser = new User(user.Email);
+            System.Diagnostics.Debug.WriteLine("Registering: " + mailUser.Username);
+
+            return true;
+        }
+    }
+}
