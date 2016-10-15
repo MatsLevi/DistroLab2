4a15e249ac8035e60011311b0939ff23492d36ea
matslevi <matslevi@kth.se>
2016-10-15 17:10:48 +0200

    did controller stuff

---
+++ DistroLab2/DistroLab2/Controllers/Mail_Controllers/MailController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DistroLab2.Controllers
+{
+    public class MailController : Controller
+    {
+         //GET: Mail
+        public ActionResult Inbox()
+        {
+            return View();
+        }
+
+        public ActionResult userSearch()
+        {
+            return View();
+        }
+    }
+}
