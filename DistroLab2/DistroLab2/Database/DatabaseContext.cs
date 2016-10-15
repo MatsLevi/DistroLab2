40ab9e7b09f2c2a520f57ccaf3afd42c4be482ab
gunnlaugur <gju@kth.se>
2016-10-15 15:46:55 +0200

    added register user functionality

---
+++ DistroLab2/DistroLab2/Database/DatabaseContext.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace DistroLab2.Database
+{
+    public class DatabaseContext : DbContext
+    {
+        public DbSet<Group> Groups { get; set; }
+        public DbSet<GroupUser> GroupUsers { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<ReceivedMessage> ReceivedMessages { get; set; }
+    }
+
+    public class Group
+    {
+        [Key]
+        public int groupId { get; set; }
+        [Required]
+        [MaxLength(120)]
+        [Index(IsUnique = true)]
+        public string name { get; set; }
+        [Required]
+        public string creator { get; set; }
+    }
+
+    public class GroupUser
+    {
+        [Key, Column(Order = 0)]
+        public int groupId { get; set; }
+        [Key, Column(Order = 1)]
+        public int userId { get; set; }
+    }
+
+    public class User
+    {
+        [Key]
+        public int userId { get; set; }
+        [Required]
+        [MaxLength(120)]
+        [Index(IsUnique = true)]
+        public string name { get; set; }
+        [Required]
+        public int removedMess { get; set; }
+        [Required]
+        public int totalMess { get; set; }
+        [Required]
+        public int readMess { get; set; }
+    }
+
+    public class Message
+    {
+        [Key]
+        public int messId { get; set; }
+        [Required]
+        public int senderId { get; set; }
+        [Required]
+        public string timestamp { get; set; }
+        [Required]
+        public string message { get; set; }
+        [Required]
+        public string title { get; set; }
+    }
+
+    public class ReceivedMessage
+    {
+        [Key, Column(Order = 0)]
+        public int messId { get; set; }
+        [Key, Column(Order = 1)]
+        public int userId { get; set; }
+        [Required]
+        public bool read { get; set; }
+    }
+}
---
+++ DistroLab2/DistroLab2/Models/Mail_Models/MailUser.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DistroLab2.Models.Mail_Models
+{
+    public class MailUser
+    {
+        private int id { get; set; }
+        private string username { get; set; }
+        private int removedMess { get; set; }
+        private int totalMess { get; set; }
+        private int readMess { get; set; }
+
+        internal MailUser()
+        {
+            id = -1;
+            username = null;
+            removedMess = -1;
+            totalMess = -1;
+            readMess = -1;
+        }
+
+        internal MailUser(string username)
+        {
+            id = -1;
+            this.username = username;
+            removedMess = 0;
+            totalMess = 0;
+            readMess = 0;
+        }
+
+        internal MailUser(int id, string username, int removedMess, int totalMess, int readMess)
+        {
+            this.id = id;
+            this.username = username;
+            this.removedMess = removedMess;
+            this.totalMess = totalMess;
+            this.readMess = readMess;
+        }
+
+        public string Username
+        {
+            get
+            {
+                return username;
+            }
+            set
+            {
+                username = value;
+            }
+        }
+    }
+}
