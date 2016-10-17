5b9bdc9ac21a33243d5242a025906e3af204f4db
gunnlaugur <gju@kth.se>
2016-10-17 13:42:59 +0200

    Added Write mail functionality

--- DistroLab2/DistroLab2/Controllers/Mail_Controllers/WriteController.cs
+++ DistroLab2/DistroLab2/Controllers/Mail_Controllers/WriteController.cs
@@ -1,8 +1,11 @@
-﻿using DistroLab2.Models.Mail_Models;
+﻿using DistroLab2.Models;
+using DistroLab2.Models.Mail_Models;
 using DistroLab2.ViewModels;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -15,6 +18,19 @@ namespace DistroLab2.Controllers.Mail_Controllers
         public ActionResult WriteMail()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult WriteHello(List<String> UserListViewModel)
+        {
+            System.Diagnostics.Debug.WriteLine("Entered Controller");
+
+            foreach(String s in UserListViewModel)
+                System.Diagnostics.Debug.WriteLine("List item: " + s);
+
+            //System.Diagnostics.Debug.WriteLine("List item: " + UserListViewModel.ToArray()[0]);
+
+            return View("WriteMail");
         }
 
         public static MailUserViewModel[] getUsers()
