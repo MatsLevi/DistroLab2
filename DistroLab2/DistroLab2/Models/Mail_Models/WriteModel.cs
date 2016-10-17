773ca7cee4184e92b7b16450d107f2c957bddd18
gunnlaugur <gju@kth.se>
2016-10-17 15:43:40 +0200

    write mail func

--- DistroLab2/DistroLab2/Models/Mail_Models/WriteModel.cs
+++ DistroLab2/DistroLab2/Models/Mail_Models/WriteModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DistroLab2.Database;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,6 +11,11 @@ namespace DistroLab2.Models.Mail_Models
         public WriteModel()
         {
 
+        }
+
+        public bool registerMail(List<String> users, string title, string message, int userId)
+        {
+            return AddMail.RegisterMail(users, title, message, userId);
         }
     }
 }
