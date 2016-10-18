c39a5adff93b29d756953d319fe5b893a8f542cc
gunnlaugur <gju@kth.se>
2016-10-18 17:58:38 +0200

    Added Inbox

--- DistroLab2/DistroLab2/Models/Mail_Models/GetMailModel.cs
+++ DistroLab2/DistroLab2/Models/Mail_Models/GetMailModel.cs
@@ -18,18 +18,37 @@ namespace DistroLab2.Models.Mail_Models
         {
             User[] dbUsers = GetUsers.getAllUsers();
 
-            if(dbUsers == null)
+            if (dbUsers == null)
             {
                 return null;
             }
 
             MailUserViewModel[] usrs = new MailUserViewModel[dbUsers.Length];
-            for(int i = 0; i < usrs.Length; i++)
+            for (int i = 0; i < usrs.Length; i++)
             {
                 usrs[i] = new MailUserViewModel(dbUsers[i].userId, dbUsers[i].name, dbUsers[i].removedMess, dbUsers[i].totalMess, dbUsers[i].readMess);
             }
 
             return usrs;
+        }
+
+        public InboxViewModel[] getAllUserMails(string username)
+        {
+            Message[] messages = GetMessages.getMessages(username);
+
+            if (messages == null)
+            {
+                return null;
+            }
+
+            InboxViewModel[] msgs = new InboxViewModel[messages.Length];
+            for (int i = 0; i < msgs.Length; i++)
+            {
+                System.Diagnostics.Debug.WriteLine("msgs length: " + msgs.Length + " title: " + messages[i].title);
+                msgs[i] = new InboxViewModel(messages[i].title, messages[i].timestamp);
+            }
+
+            return msgs;
         }
     }
 }
